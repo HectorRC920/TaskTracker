9ad3232bfccc2fef5b89a54f71b4f1b2961d507f
HectorRC920 <hector.rubio@sokolabs.com>
2022-04-19 19:47:42 -0600

    add: footer and header

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+    
+    return (
+        <React.Fragment>
+            <footer className='Footer'>
+                <h5>Made with 💚  By &nbsp;  
+                     <a className='social_link' target={"_blank"} href='https://github.com/HectorRC920'>Héctor Rubio</a>
+                </h5>
+            </footer>
+        </React.Fragment>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+const Header = () => {
+    return (
+        <React.Fragment>
+            <header className='Header'>
+                
+            </header>
+        </React.Fragment>
+    );
+}
+export default Header;
